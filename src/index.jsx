e3d0201c5d9911b9ff389228c76fae33fed54b87
Abdul Halim Arif <53304120+ahadaulay@users.noreply.github.com>
2024-09-29 00:08:35 +0700

    save project

---
+++ src/index.jsx
@@ -0,0 +1,7 @@
+import { createRoot } from "react-dom/client";
+import ContactApp from "./components/ContactApp";
+
+import "./style.css";
+
+const root = createRoot(document.getElementById("root"));
+root.render(<ContactApp />);
